4dd851185ef38d0ff790994cf2b70d857704f399
WinNode <117538854+Winnode@users.noreply.github.com>
2024-03-23 22:05:57 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,29 @@
+# GetGrass Crooter
+## Installation
+#### Install python3
+- For windows: https://www.python.org/ftp/python/3.12.2/python-3.12.2-amd64.exe 
+- For linux distros: https://dev.to/ivayloiv/install-latest-python-version-on-any-linux-distro-5gc3
+#### Python3 module Installation
+- Install from requirements.txt
+```pip install -r requirements.txt```
+### REGISTER: https://app.getgrass.io/register/?referralCode=PnmuSjrqxyxvZsk ( Please use this link instead! xD )
+## HOW TO USE
+#### How to get your grass userid
+- Login to https://app.getgrass.io
+- Press f12 go to console, then type ```allow pasting``` insert to console
+![0001](https://github.com/im-hanzou/getgrass_bot/blob/main/pasting.JPG)
+
+- Then insert this code to console
+```localStorage.getItem('userId')```
+![0001](https://github.com/im-hanzou/getgrass_bot/blob/main/userid.JPG)
+#### Usage command
+- Open file ```proxy_list.txt``` and insert your proxies
+- Run ```python run.py``` and insert your userid
+- <img src="https://github.com/im-hanzou/getgrass_bot/blob/main/run.png" width=500>
+<!--
+- For multiple accounts and for each of them one proxy
+-  insert your accounts user ids to user_id.txt and insert your proxies to proxy_list(all).txt
+- Then ```python foreachuser_id_proxy.py```
+-->
+
+
---
+++ run.py
@@ -0,0 +1,88 @@
+#script for 1 user_id and a whole bunch of proxies
+
+import asyncio
+import random
+import ssl
+import json
+import time
+import uuid
+from loguru import logger
+from websockets_proxy import Proxy, proxy_connect
+from fake_useragent import UserAgent
+
+user_agent = UserAgent()
+random_user_agent = user_agent.random
+
+
+async def connect_to_wss(socks5_proxy, user_id):
+    device_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, socks5_proxy))
+    logger.info(device_id)
+    while True:
+        try:
+            await asyncio.sleep(random.randint(1, 10) / 10)
+            custom_headers = {
+                "User-Agent": random_user_agent
+            }
+            ssl_context = ssl.create_default_context()
+            ssl_context.check_hostname = False
+            ssl_context.verify_mode = ssl.CERT_NONE
+            uri = "wss://proxy.wynd.network:4650/"
+            server_hostname = "proxy.wynd.network"
+            proxy = Proxy.from_url(socks5_proxy)
+            async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname,
+                                     extra_headers=custom_headers) as websocket:
+                async def send_ping():
+                    while True:
+                        send_message = json.dumps(
+                            {"id": str(uuid.uuid4()), "version": "1.0.0", "action": "PING", "data": {}})
+                        logger.debug(send_message)
+                        await websocket.send(send_message)
+                        await asyncio.sleep(20)
+
+                # asyncio.create_task(send_http_request_every_10_seconds(socks5_proxy, device_id))
+                await asyncio.sleep(1)
+                asyncio.create_task(send_ping())
+
+                while True:
+                    response = await websocket.recv()
+                    message = json.loads(response)
+                    logger.info(message)
+                    if message.get("action") == "AUTH":
+                        auth_response = {
+                            "id": message["id"],
+                            "origin_action": "AUTH",
+                            "result": {
+                                "browser_id": device_id,
+                                "user_id": user_id,
+                                "user_agent": custom_headers['User-Agent'],
+                                "timestamp": int(time.time()),
+                                "device_type": "extension",
+                                "version": "3.3.2"
+                            }
+                        }
+                        logger.debug(auth_response)
+                        await websocket.send(json.dumps(auth_response))
+
+                    elif message.get("action") == "PONG":
+                        pong_response = {"id": message["id"], "origin_action": "PONG"}
+                        logger.debug(pong_response)
+                        await websocket.send(json.dumps(pong_response))
+        except Exception as e:
+            logger.error(e)
+            logger.error(socks5_proxy)
+
+
+async def main():
+    #find user_id on the site in conlose localStorage.getItem('userId') (if you can't get it, write allow pasting)
+    _user_id = input('Please Enter your user ID: ')
+    #put the proxy in a file in the format socks5://username:password@ip:port or socks5://ip:port
+    with open('proxy_list.txt', 'r') as file:
+        socks5_proxy_list = file.read().splitlines()
+    
+    tasks = [asyncio.ensure_future(connect_to_wss(i, _user_id)) for i in socks5_proxy_list]
+    await asyncio.gather(*tasks)
+
+
+if __name__ == '__main__':
+    #letsgo
+    asyncio.run(main())
